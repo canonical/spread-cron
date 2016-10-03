49a9552ac261bdb835b36e6042f44b1aa007a484
Federico Gimenez <fgimenez@coit.es>
2016-10-03 14:40:11 +0200

    only check branches if the credentials are present

--- cron.sh
+++ cron.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-BASE_DIR="$SNAP_DATA/spread-cron"
+BASE_DIR="$SNAP_COMMON/spread-cron"
 STEP=3000
+FINAL_TIME=0
+INITIAL_TIME=0
 
 while true; do
     if [ ! -d "$BASE_DIR" ]; then
-        git clone git://github.com/snapcore/spread-cron
+        git clone git://github.com/snapcore/spread-cron "$BASE_DIR"
     fi
     cd "$BASE_DIR" || exit
 
     # run checker on each branch
-    initial_time=$SECONDS
-    for remote in $(git branch -r); do
-        if [ "$remote" != "origin/HEAD" ] && [ "$remote" != "origin/master" ] && [ "$remote" != "->" ]; then
-            branch="${remote#origin/}"
-            git branch --track "$branch" "$remote";
-            git checkout "$branch"
-            git pull
-            $BASE_DIR/checker/run
-        fi
-    done
-    final_time=$SECONDS
-
-    sleep $((STEP - final_time + initial_time))
+    if [ ! -f "$SNAP_COMMON/git-credentials" ]; then
+        echo "No credentials file found, please run 'snap set spread-cron username=<username> password=<password>' with valid credentials of a github user with push priveleges for the snapcore/spread-cron repository."
+    else
+        INITIAL_TIME=$SECONDS
+        for remote in $(git branch -r); do
+            if [ "$remote" != "origin/HEAD" ] && [ "$remote" != "origin/master" ] && [ "$remote" != "->" ]; then
+                branch="${remote#origin/}"
+                git branch --track "$branch" "$remote";
+                git checkout "$branch"
+                git pull
+                $BASE_DIR/checker/run
+            fi
+        done
+        FINAL_TIME=$SECONDS
+    fi
+    sleep $((STEP - FINAL_TIME + INITIAL_TIME))
 done
