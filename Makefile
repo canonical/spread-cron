f2101d2311ac17a34fd50d573c4c1a36b3060d6c
Federico Gimenez <fgimenez@coit.es>
2016-10-05 16:46:15 +0200

    build and add curl deps to have git's https transport in place

--- Makefile
+++ Makefile
@@ -1,9 +1,18 @@
 # -*- Mode: Makefile; indent-tabs-mode:t; tab-width: 4 -*-
 
+TEMPDIR := $(shell mktemp -d)
+
 all:
 
 install:
-	mkdir -p $(DESTDIR)/bin $(DESTDIR)/meta/hooks
+	wget https://www.kernel.org/pub/software/scm/git/git-2.9.3.tar.gz -O $(TEMPDIR)/git.tar.gz
+	cd $(TEMPDIR) && tar -xf git.tar.gz
+	# target curl
+	cd $(TEMPDIR)/git-2.9.3 && LDFLAGS="-L usr/lib/x86_64-linux-gnu" ./configure --prefix=/usr --with-curl=/usr/bin && make && make install
+
+	mkdir -p $(DESTDIR)/bin
 	cp -a cron.sh $(DESTDIR)/bin/cron
-	cp -a meta/hooks/* $(DESTDIR)/meta/hooks
 	chmod a+x $(DESTDIR)/bin/cron
+
+	mkdir -p $(DESTDIR)/meta/hooks
+	cp -a meta/hooks/* $(DESTDIR)/meta/hooks
--- cron.sh
+++ cron.sh
@@ -6,15 +6,15 @@ FINAL_TIME=0
 INITIAL_TIME=0
 
 while true; do
-    if [ ! -d "$BASE_DIR" ]; then
-        git clone git://github.com/snapcore/spread-cron "$BASE_DIR"
-    fi
-    cd "$BASE_DIR" || exit
-
     # run checker on each branch
     if [ ! -f "$SNAP_COMMON/git-credentials" ]; then
         echo "No credentials file found, please run 'snap set spread-cron username=<username> password=<password>' with valid credentials of a github user with push priveleges for the snapcore/spread-cron repository."
     else
+        if [ ! -d "$BASE_DIR" ]; then
+            git clone https://github.com/snapcore/spread-cron.git "$BASE_DIR"
+        fi
+        cd "$BASE_DIR" || exit
+
         INITIAL_TIME=$SECONDS
         for remote in $(git branch -r); do
             if [ "$remote" != "origin/HEAD" ] && [ "$remote" != "origin/master" ] && [ "$remote" != "->" ]; then
