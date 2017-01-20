9c3cb4b6835921582855b27730dc7979a9c3ebae
Federico Gimenez <fgimenez@coit.es>
2017-01-20 14:28:47 +0100

    use spread.yaml from master

--- actions.sh
+++ actions.sh
@@ -3,8 +3,8 @@
 # keep tests from master, checkout 2.21 and put tests from master in place
 (
     cd target || exit
-    cp -ar tests ..
+    cp -ar ./tests ./spread.yaml ..
     git fetch
     git checkout release/2.21
-    rm -rf tests && mv ../tests .
+    rm -rf ./tests ./spread.yaml && mv ../tests ../spread.yaml .
 )
