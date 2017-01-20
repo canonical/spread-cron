afa095396795f94d67f56362263dc42f82e79601
Federico Gimenez <fgimenez@coit.es>
2017-01-20 11:47:03 +0100

    initial version [skip ci]

--- actions.sh
+++ actions.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-cp -ar custom/. target/
+# keep tests from master, checkout 2.21 and put tests from master in place
+(
+    cd target || exit
+    cp -ar tests ..
+    git fetch
+    git checkout release/2.21
+    rm -rf tests && mv ../tests .
+)
