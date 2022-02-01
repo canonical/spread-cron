6cce996cd9201413a49979bca2588e249b902652
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-02-01 09:35:16 -0300

    using spread-debug to update spread images

--- lib/run_google_action.sh
+++ lib/run_google_action.sh
@@ -18,6 +18,7 @@ CURR_DIR="$(pwd)"
 TMP_IMAGE_ID="$(date +%N)"
 SNAPD_DIR="$CURR_DIR/snapd"
 SPREAD_IMAGES_DIR="$CURR_DIR/spread-images"
+SPREAD_BIN=spread-debug
 
 # Prepare spread-images project
 git clone https://github.com/snapcore/spread-images "$SPREAD_IMAGES_DIR"
@@ -36,17 +37,17 @@ git clone https://github.com/snapcore/snapd.git "$SNAPD_DIR"
 cd "$SPREAD_IMAGES_DIR"
 if [ "$RUN_SNAPD" = "true" ]; then
 	echo "Running spread-images task and creating tmp image"
-    if ! SPREAD_TMP_IMAGE_ID="$TMP_IMAGE_ID" spread "google:${SOURCE_SYSTEM}:tasks/google/${GOOGLE_ACTION}/${GOOGLE_TASK}"; then
+    if ! SPREAD_TMP_IMAGE_ID="$TMP_IMAGE_ID" "$SPREAD_BIN" "google:${SOURCE_SYSTEM}:tasks/google/${GOOGLE_ACTION}/${GOOGLE_TASK}"; then
 		echo "Spread images task failed, exiting..."
 		exit 1
 	fi
 else
 	echo "Running spread-images task and creating final image"
-	if ! spread "google:${SOURCE_SYSTEM}:tasks/google/${GOOGLE_ACTION}/${GOOGLE_TASK}"; then
+	if ! "$SPREAD_BIN" "google:${SOURCE_SYSTEM}:tasks/google/${GOOGLE_ACTION}/${GOOGLE_TASK}"; then
 		echo "Spread images task failed, exiting..."
 		exit 1
 	fi
-	SPREAD_TARGET_SYSTEM="$TARGET_SYSTEM" SPREAD_SYSTEM_BACKUPS="$BACKUPS" spread "google:${TARGET_SYSTEM}:tasks/google/common/clean-old-images"
+	SPREAD_TARGET_SYSTEM="$TARGET_SYSTEM" SPREAD_SYSTEM_BACKUPS="$BACKUPS" "$SPREAD_BIN" "google:${TARGET_SYSTEM}:tasks/google/common/clean-old-images"
 	exit
 fi
 
@@ -59,16 +60,16 @@ fi
 
 cd "$SNAPD_DIR"
 PUBLISH_IMAGE=false
-if spread "google:${TARGET_SYSTEM}"; then
+if "$SPREAD_BIN" "google:${TARGET_SYSTEM}"; then
 	PUBLISH_IMAGE=true
 fi
 
 # Publish the final image and clean the temporal one
 cd "$SPREAD_IMAGES_DIR"
 if [ "$PUBLISH_IMAGE" = true ]; then
-	SPREAD_TMP_IMAGE_ID="$TMP_IMAGE_ID" SPREAD_TARGET_SYSTEM="$TARGET_SYSTEM" spread "google:${SOURCE_SYSTEM}:tasks/google/common/publish-tmp-image"
-	SPREAD_TARGET_SYSTEM="$TARGET_SYSTEM" SPREAD_SYSTEM_BACKUPS="$BACKUPS" spread "google:${TARGET_SYSTEM}:tasks/google/common/clean-old-images"
+	SPREAD_TMP_IMAGE_ID="$TMP_IMAGE_ID" SPREAD_TARGET_SYSTEM="$TARGET_SYSTEM" "$SPREAD_BIN" "google:${SOURCE_SYSTEM}:tasks/google/common/publish-tmp-image"
+	SPREAD_TARGET_SYSTEM="$TARGET_SYSTEM" SPREAD_SYSTEM_BACKUPS="$BACKUPS" "$SPREAD_BIN" "google:${TARGET_SYSTEM}:tasks/google/common/clean-old-images"
 else
-	SPREAD_TMP_IMAGE_ID="$TMP_IMAGE_ID" spread "google:${SOURCE_SYSTEM}:tasks/google/common/remove-tmp-image"
+	SPREAD_TMP_IMAGE_ID="$TMP_IMAGE_ID" "$SPREAD_BIN" "google:${SOURCE_SYSTEM}:tasks/google/common/remove-tmp-image"
 	exit 1
 fi
